278c7d7c42d49216fd813ef6ce38de929c5d4a27
andmil04 <andrew.miller@roberthalf.com>
2017-10-10 12:09:43 -0700

    added es6 classes sample

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,35 @@
+
+class Person {
+  constructor(name = 'Anonymous', age = 0){
+    this.name = name ;
+    this.age = age;
+  }
+  getGretting() {
+    //return 'Hi. I am ' + this.name + '!';
+    return `Hi. I am ${this.name} !`;
+  }
+  getDescription(){
+    return `${this.name} is ${this.age} year(s) old.`;
+  }
+}
+//how to use Person items within Student
+class Student extends Person{
+  constructor( name, age, major){
+    //super is internal method used to call parent constructor
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor(){
+    return !!this.major;
+  }
+  getDescription(){
+    return 'testing';
+  }
+}
+
+const me = new Student('Andrew Miller', 42, 'Computer Sicence');
+
+console.log(me.getDescription());
+
+const other = new Student();
+console.log(other.getDescription());
