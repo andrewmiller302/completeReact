f7bb86a70af24c1b5b9703c94a36a31a6a3f10cf
andmil04 <andrew.miller@roberthalf.com>
2017-10-19 16:57:28 -0700

    added new items

--- src/app.js
+++ src/app.js
@@ -11,10 +11,25 @@ class IndecisionApp extends React.Component {
     };
   }
   componentDidMount(){
-    console.log('fetching data')
+    try{
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+      if(options){
+        this.setState(()=> ({ options: options }));
+      }
+    } catch (e){
+      // if not valid JSON Do Nothing at all 
+    }
+    
   }
+
+
   componentDidUpdate(prevProps, prevState){
-    console.log('componentDidUpdate!')
+    if (prevState.options.length !== this.state.options.length){
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+    
   }
   componentWillUnmount(){
     console.log('componentWillUnmount')
@@ -112,7 +127,8 @@ const Options = (props) => {
   return(
     <div>
     <button onClick={props.handleDeleteOptions}>Remove all</button>
-      {
+    {props.options.length === 0 && <p>Please add an option to get started!</p>}  
+    {
         props.options.map((option)=> (
           <Option 
             key={option} 
@@ -178,6 +194,10 @@ class AddOption extends React.Component {
     const error = this.props.handleAddOption(option);
     
     this.setState(() =>({ error: error}));
+
+    if (!error){
+      e.target.elements.option.value = '';
+    }
   }
   render() {
     return (
