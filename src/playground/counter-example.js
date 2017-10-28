bab958dda1c85b409c1fa333cde7fcaf54db9043
andrewmiller302 <andrewmiller302@comcast.net>
2017-10-28 12:35:46 -0700

    update webpack and package json

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -6,9 +6,28 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset =this.handleReset.bind(this);
         this.state = {
-            count: props.count
+            count: 0
         };
     }
+    
+   componentDidMount(){
+       const stringCount = localStorage.getItem('count');
+       const count = parseInt(stringCount, 10);
+
+       if (!isNaN(count)){
+           this.setState(() => ({count:count}));
+       }
+   }
+      
+    componentDidUpdate(prevProps, prevState){
+       if (prevState.count !== this.state.count){
+           localStorage.setItem('count', this.state.count);
+       }
+        
+      }
+      componentWillUnmount(){
+        console.log('componentWillUnmount')
+      }
     
     handleAddOne() {
         this.setState((prevState) =>{
@@ -44,9 +63,7 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps = {
-    count: 0
-};
+
 
 ReactDOM.render(<Counter count ={-10}/>, document.getElementById('app'));
 
